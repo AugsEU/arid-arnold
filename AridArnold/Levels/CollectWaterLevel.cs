cec668dc8ce538ff730f2ebc3514937dfe596ece
adahlkvist <AugustDahlkvist@gmail.com>
2022-05-08 14:00:55 +0100

    Move CollectWaterLevel to its own file.

---
+++ AridArnold/Levels/CollectWaterLevel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace AridArnold.Levels
+{
+    class CollectWaterLevel : Level
+    {
+        int mNumWaterNeeded;
+        public CollectWaterLevel(string levelName, int numNeeded) : base(levelName)
+        {
+            mNumWaterNeeded = numNeeded;
+        }
+
+        public override LevelStatus Update(GameTime gameTime)
+        {
+            if (CollectibleManager.I.GetCollected(CollectibleType.WaterBottle) >= mNumWaterNeeded)
+            {
+                mLevelStatus = LevelStatus.Win;
+            }
+
+            return mLevelStatus;
+        }
+    }
+}
--- AridArnold/Levels/Level.cs
+++ AridArnold/Levels/Level.cs
@@ -41,22 +41,5 @@ namespace AridArnold.Levels
         public abstract LevelStatus Update(GameTime gameTime);
     }
 
-    class CollectWaterLevel : Level
-    {
-        int mNumWaterNeeded;
-        public CollectWaterLevel(string levelName, int numNeeded) : base(levelName)
-        {
-            mNumWaterNeeded = numNeeded;
-        }
 
-        public override LevelStatus Update(GameTime gameTime)
-        {
-            if(CollectibleManager.I.GetCollected(CollectibleType.WaterBottle) >= mNumWaterNeeded)
-            {
-                mLevelStatus = LevelStatus.Win;
-            }
-
-            return mLevelStatus;
-        }
-    }
 }
