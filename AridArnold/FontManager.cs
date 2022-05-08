f1fbea525fa55187c90ab590b6ee40bd5fbe3905
adahlkvist <AugustDahlkvist@gmail.com>
2022-05-08 22:56:04 +0100

    Add font manager

---
+++ AridArnold/FontManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace AridArnold
+{
+    internal class FontManager : Singleton<FontManager>
+    {
+        Dictionary<string, SpriteFont> mFonts = new Dictionary<string, SpriteFont>();
+
+        public void LoadAllFonts(ContentManager content)
+        {
+            mFonts.Add("Pixica-24", content.Load<SpriteFont>("Fonts/Pixica"));
+        }
+
+        public SpriteFont GetFont(string key)
+        {
+            return mFonts.GetValueOrDefault(key);
+        }
+    }
+}
