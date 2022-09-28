9e63e72305f26fb38a218261dbb20fc83f32bc6f
adahlkvist <AugustDahlkvist@gmail.com>
2022-09-28 22:40:44 +0100

    Move worldData struct to it's own file

---
+++ AridArnold/Files/WorldData.cs
@@ -0,0 +1,30 @@
+﻿namespace AridArnold
+{
+	/// <summary>
+	/// Data about the world(set of levels)
+	/// </summary>
+	struct WorldData
+	{
+		public string name;
+
+		//Style
+		public Color worldColor;
+		public string wallTexture;
+		public string platformTexture;
+
+		//Levels
+		public Level[] mLevels;
+
+		public WorldData(string _name, Color _worldColor, string _wallTexture, string _platformTexture, Level[] levels)
+		{
+			name = _name;
+
+			//Style
+			worldColor = _worldColor;
+			wallTexture = _wallTexture;
+			platformTexture = _platformTexture;
+
+			mLevels = levels;
+		}
+	}
+}
