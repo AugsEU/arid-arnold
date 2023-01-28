423dd4ab5461fa6e2f2c4c5d5ef090faf146e58b
adahlkvist <AugustDahlkvist@gmail.com>
2023-01-28 15:48:46 +0000

    Fix mirror tile displacement

--- AridArnold/Tiles/MirrorTile.cs
+++ AridArnold/Tiles/MirrorTile.cs
@@ -37,56 +37,67 @@
 		/// <param name="bounds">Our tile bounds</param>
 		public override void OnEntityIntersect(Entity entity)
 		{
+			float diffThresh = 4.0f;
+
 			if (entity is PlatformingEntity)
 			{
-				Rect2f bounds = GetBounds();
+				Rect2f ourBounds = GetBounds();
 
 				PlatformingEntity platformingEntity = (PlatformingEntity)entity;
+
+				Rect2f entityBounds = entity.ColliderBounds();
+				float entityHeight = entityBounds.Height;
+				float entityWidth = entityBounds.Width;
+
+				Vector2 entityCentre = entityBounds.Centre;
+				Vector2 ourCentre = ourBounds.Centre;
+				float xDiff = Math.Abs(entityCentre.X - ourCentre.X);
+				float yDiff = Math.Abs(entityCentre.Y - ourCentre.Y);
 
 				switch (mRotation)
 				{
 					case CardinalDirection.Up:
-						if (platformingEntity.GetGravityDir() != CardinalDirection.Down)
+						if (platformingEntity.GetGravityDir() != CardinalDirection.Down && xDiff < diffThresh)
 						{
 							if (platformingEntity.GetGravityDir() == CardinalDirection.Left || platformingEntity.GetGravityDir() == CardinalDirection.Right)
 							{
 								platformingEntity.SetWalkDirection(WalkDirection.None);
 							}
 							platformingEntity.SetGravity(CardinalDirection.Down);
-							platformingEntity.ShiftPosition(new Vector2(0.0f, -(bounds.Height + 16.0f)));
+							platformingEntity.ShiftPosition(new Vector2(0.0f, -(ourBounds.Height + entityHeight)));
 						}
 						break;
 					case CardinalDirection.Right:
-						if (platformingEntity.GetGravityDir() != CardinalDirection.Left)
+						if (platformingEntity.GetGravityDir() != CardinalDirection.Left && yDiff < diffThresh)
 						{
 							if (platformingEntity.GetGravityDir() == CardinalDirection.Up || platformingEntity.GetGravityDir() == CardinalDirection.Down)
 							{
 								platformingEntity.SetWalkDirection(WalkDirection.None);
 							}
 							platformingEntity.SetGravity(CardinalDirection.Left);
-							platformingEntity.ShiftPosition(new Vector2(bounds.Width + 16.0f, 0.0f));
+							platformingEntity.ShiftPosition(new Vector2(ourBounds.Width + entityWidth, 0.0f));
 						}
 						break;
 					case CardinalDirection.Down:
-						if (platformingEntity.GetGravityDir() != CardinalDirection.Up)
+						if (platformingEntity.GetGravityDir() != CardinalDirection.Up && xDiff < diffThresh)
 						{
 							if (platformingEntity.GetGravityDir() == CardinalDirection.Left || platformingEntity.GetGravityDir() == CardinalDirection.Right)
 							{
 								platformingEntity.SetWalkDirection(WalkDirection.None);
 							}
 							platformingEntity.SetGravity(CardinalDirection.Up);
-							platformingEntity.ShiftPosition(new Vector2(0.0f, bounds.Height + 16.0f));
+							platformingEntity.ShiftPosition(new Vector2(0.0f, ourBounds.Height + entityHeight));
 						}
 						break;
 					case CardinalDirection.Left:
-						if (platformingEntity.GetGravityDir() != CardinalDirection.Right)
+						if (platformingEntity.GetGravityDir() != CardinalDirection.Right && yDiff < diffThresh)
 						{
 							if (platformingEntity.GetGravityDir() == CardinalDirection.Up || platformingEntity.GetGravityDir() == CardinalDirection.Down)
 							{
 								platformingEntity.SetWalkDirection(WalkDirection.None);
 							}
 							platformingEntity.SetGravity(CardinalDirection.Right);
-							platformingEntity.ShiftPosition(new Vector2(-(bounds.Width + 16.0f), 0.0f));
+							platformingEntity.ShiftPosition(new Vector2(-(ourBounds.Width + entityWidth), 0.0f));
 						}
 						break;
 				}
