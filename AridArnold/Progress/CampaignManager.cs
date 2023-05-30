10bddd93f69949ad059082dab098d9c58a569a4e
adahlkvist <AugustDahlkvist@gmail.com>
2023-05-30 22:57:55 +0100

    206 ideas

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -72,7 +72,7 @@ namespace AridArnold
 #if DEBUG_LOADER
 			CollectableManager.I.ChangePermanentItem(0x0300, 10);
 			//QueueLoadSequence(new HubDirectLoader(201));
-			QueueLoadSequence(new LevelDirectLoader(205));
+			QueueLoadSequence(new LevelDirectLoader(206));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
 #endif
