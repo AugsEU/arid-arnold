d2b547c13909856f0188fbac3e92df0dbe969510
adahlkvist <AugustDahlkvist@gmail.com>
2023-06-20 21:33:09 +0100

    Add RobotoSpawner files

---
+++ AridArnold/Tiles/Electric/RobotoSpawner.cs
@@ -0,0 +1,15 @@
+﻿namespace AridArnold
+{
+	internal class RobotoSpawner : SquareTile
+	{
+		public RobotoSpawner(Vector2 position) : base(position)
+		{
+		}
+
+		public override void LoadContent()
+		{
+			mTexture = MonoData.I.MonoGameLoad<Texture2D>("Tiles/Lab/")
+			base.LoadContent();
+		}
+	}
+}
