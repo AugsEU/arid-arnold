fdccb7fec9dd01cb873944e967fe976b7d3d2e17
adahlkvist <AugustDahlkvist@gmail.com>
2023-11-17 21:29:06 +0100

    Tweak backgrounds for WW7

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -74,9 +74,9 @@ namespace AridArnold
 #if DEBUG_LOADER
 			CollectableManager.I.ChangePermanentItem(0x0300, 10);
 			CollectableManager.I.ChangePermanentItem(0x0000, 100);
-			TimeZoneManager.I.SetCurrentTimeZoneAndAge(0, 0);
-			//QueueLoadSequence(new HubDirectLoader(701));
-			QueueLoadSequence(new LevelDirectLoader(709));
+			TimeZoneManager.I.SetCurrentTimeZoneAndAge(1, 0);
+			QueueLoadSequence(new HubDirectLoader(701));
+			//QueueLoadSequence(new LevelDirectLoader(712));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
 #endif
