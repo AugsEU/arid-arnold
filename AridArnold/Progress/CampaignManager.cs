73ccec9cdb4a68882cdb7442bb397cb544de889e
adahlkvist <AugustDahlkvist@gmail.com>
2024-01-15 21:45:11 +0100

    Add metadata for heaven world

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -76,7 +76,7 @@ namespace AridArnold
 			CollectableManager.I.ChangePermanentItem(0x0000, 100);
 			TimeZoneManager.I.SetCurrentTimeZoneAndAge(2, 1);
 			//QueueLoadSequence(new HubDirectLoader(801));
-			QueueLoadSequence(new LevelDirectLoader(808));
+			QueueLoadSequence(new LevelDirectLoader(901));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
 #endif
