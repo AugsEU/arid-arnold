7a9a44defc4000322ffcb07af75b7eb5f8511e32
adahlkvist <AugustDahlkvist@gmail.com>
2024-01-29 22:50:40 +0100

    904 first draft

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -76,7 +76,7 @@ namespace AridArnold
 			CollectableManager.I.ChangePermanentItem(0x0000, 100);
 			TimeZoneManager.I.SetCurrentTimeZoneAndAge(-1, 0);
 			//QueueLoadSequence(new HubDirectLoader(801));
-			QueueLoadSequence(new LevelDirectLoader(903));
+			QueueLoadSequence(new LevelDirectLoader(904));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
 #endif
