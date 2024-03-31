859bff31e5f9c74ef4632b6dad8121e3d2ba261f
adahlkvist <AugustDahlkvist@gmail.com>
2024-03-31 20:51:32 +0200

    Add more stuff to the lab
    First draft for the lab is complete

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -78,8 +78,8 @@ namespace AridArnold
 #if DEBUG_LOADER
 			CollectableManager.I.ChangePermanentItem(0x0300, 10);
 			CollectableManager.I.ChangePermanentItem(0x0000, 100);
-			TimeZoneManager.I.SetCurrentTimeZoneAndAge(1, 1);
-			QueueLoadSequence(new HubDirectLoader(403));
+			TimeZoneManager.I.SetCurrentTimeZoneAndAge(0, 0);
+			QueueLoadSequence(new HubDirectLoader(401));
 			//QueueLoadSequence(new LevelDirectLoader(404));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
