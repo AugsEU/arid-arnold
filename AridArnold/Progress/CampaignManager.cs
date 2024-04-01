47212f01be25fb41e28de097c512727ca5876f24
adahlkvist <AugustDahlkvist@gmail.com>
2024-04-01 15:12:49 +0200

    Mirror levels

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -79,8 +79,8 @@ namespace AridArnold
 			CollectableManager.I.ChangePermanentItem(0x0300, 10);
 			CollectableManager.I.ChangePermanentItem(0x0000, 100);
 			TimeZoneManager.I.SetCurrentTimeZoneAndAge(0, 0);
-			QueueLoadSequence(new HubDirectLoader(501));
-			//QueueLoadSequence(new LevelDirectLoader(501));
+			//QueueLoadSequence(new HubDirectLoader(503));
+			QueueLoadSequence(new LevelDirectLoader(501));
 #else
 			QueueLoadSequence(new HubDirectLoader(mMetaData.GetStartRoomID()));
 #endif
