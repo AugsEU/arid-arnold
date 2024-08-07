04030c67f81d2c699fd8b89128199e9a5a858cd6
adahlkvist <AugustDahlkvist@gmail.com>
2024-08-07 22:59:58 +0200

    Populate heaven world with level doors

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -65,9 +65,9 @@
 				CollectableManager.I.IncPermanentCount(0x0300, 100);
 				CollectableManager.I.IncPermanentCount(0x0000, 100);
 				//CollectableManager.I.IncPermanentCount((UInt16)CollectableCategory.WaterBottle, 100);
-				TimeZoneManager.I.SetCurrentTimeZoneAndAge(2, 1);
-				QueueLoadSequence(new HubDirectLoader(801));
-				//QueueLoadSequence(new LevelDirectLoader(810));
+				TimeZoneManager.I.SetCurrentTimeZoneAndAge(-1, 0);
+				//QueueLoadSequence(new HubDirectLoader(903));
+				QueueLoadSequence(new LevelDirectLoader(916));
 
 				FlagsManager.I.SetFlag(FlagCategory.kKeyItems, (UInt32)KeyItemFlagType.kGatewayKey, true);
 				FlagsManager.I.SetFlag(FlagCategory.kKeyItems, (UInt32)KeyItemFlagType.kRippedJeans, true);
