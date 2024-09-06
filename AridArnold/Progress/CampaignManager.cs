d490a970dcdd621e59f21f47b37038a144c93487
adahlkvist <AugustDahlkvist@gmail.com>
2024-09-06 20:56:17 +0200

    WW7 hub tweak

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -72,9 +72,9 @@
 					CollectableManager.I.IncPermanentCount(coinID, 100);
 				}
 
-				TimeZoneManager.I.SetCurrentTimeZoneAndAge(0, 0);
+				TimeZoneManager.I.SetCurrentTimeZoneAndAge(1, 0);
 
-				QueueLoadSequence(new HubDirectLoader(601));
+				QueueLoadSequence(new HubDirectLoader(701));
 				//QueueLoadSequence(new LevelDirectLoader(60000));
 
 				FlagsManager.I.SetFlag(FlagCategory.kKeyItems, (UInt32)KeyItemFlagType.kGatewayKey, true);
