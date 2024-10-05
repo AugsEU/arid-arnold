f7de5055626f0063812affb34a87bf9d44eec00f
adahlkvist <AugustDahlkvist@gmail.com>
2024-10-05 15:23:03 +0200

    v1.1 publish

--- AridArnold/Progress/CampaignManager.cs
+++ AridArnold/Progress/CampaignManager.cs
@@ -73,10 +73,10 @@
 					CollectableManager.I.IncPermanentCount(coinID, 100);
 				}
 
-				TimeZoneManager.I.SetCurrentTimeZoneAndAge(2, 1);
+				TimeZoneManager.I.SetCurrentTimeZoneAndAge(0, 0);
 
-				QueueLoadSequence(new HubDirectLoader(801));
-				//QueueLoadSequence(new LevelDirectLoader(101));
+				//QueueLoadSequence(new HubDirectLoader(801));
+				QueueLoadSequence(new LevelDirectLoader(908));
 
 				FlagsManager.I.SetFlag(FlagCategory.kKeyItems, (UInt32)KeyItemFlagType.kGatewayKey, true);
 				//FlagsManager.I.SetFlag(FlagCategory.kKeyItems, (UInt32)KeyItemFlagType.kRippedJeans, true);
